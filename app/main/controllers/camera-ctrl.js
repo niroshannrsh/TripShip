938a011cacc03fa76476f17ce2dde04b750bc634
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-12-23 13:18:15 -0500

    cordova camere

--- app/main/controllers/camera-ctrl.js
+++ app/main/controllers/camera-ctrl.js
@@ -27,8 +27,35 @@ angular.module('main')
 //
 // });
 
-.controller('CameraCtrl', function ($scope) {
+.controller('CameraCtrl', function ($scope,$cordovaCamera) {
+     var upload =function (){
+
+     };
+
+     var getBlob = function(base64Data, contentType) {
+            contentType = contentType || '';
+            var sliceSize = 1024;
+            var byteCharacters = atob(base64Data);
+            var bytesLength = byteCharacters.length;
+            var slicesCount = Math.ceil(bytesLength / sliceSize);
+            var byteArrays = new Array(slicesCount);
+            for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+                var begin = sliceIndex * sliceSize;
+                var end = Math.min(begin + sliceSize, bytesLength);
+
+                var bytes = new Array(end - begin);
+                for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+                    bytes[i] = byteCharacters[offset].charCodeAt(0);
+                }
+                byteArrays[sliceIndex] = new Uint8Array(bytes);
+            }
+            return new Blob(byteArrays, { type: contentType });
+        };
+
+
      $scope.takePic = function() {
+
+    /*
         var options =   {
             quality: 50,
             destinationType: Camera.DestinationType.FILE_URI,
@@ -36,6 +63,46 @@ angular.module('main')
             encodingType: 0     // 0=JPG 1=PNG
         }
         navigator.camera.getPicture(onSuccess,onFail,options);
+
+    */
+         var options = {
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 540,
+        targetHeight: 540,
+        mediaType: Camera.MediaType.PICTURE,
+        correctOrientation: true,
+        saveToPhotoAlbum: false
+      };
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        /*
+        $scope.newImage = {
+          type: "image",
+          created_at: new Date(),
+          updated_at: new Date(),
+          url: null,
+          user: {
+            email: $scope.currentUser.email,
+            name: $scope.currentUser.name,
+            user_id: $scope.currentUser.id
+          }
+        };
+        */
+
+        //$scope.addImage($scope.newImage);
+
+        upload(getBlob(imageData, 'image/jpeg')).then(
+          function(data) {
+            $scope.newImage.url = '/uploads/' + keyName;
+          },
+          function(err) {
+            console.log(err);
+          });
+      }, function(err) {
+        console.log(err);
+      });
     }
     var onSuccess = function(FILE_URI) {
         console.log(FILE_URI);
